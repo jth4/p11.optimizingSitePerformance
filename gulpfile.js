c2b9acb0cb9c7e60fad8d822e0225af9eb4f6f8e
jth4 <ohi@jth4.com>
2017-06-26 13:07:09 -0700

    Optimize - 1st pass
    Webfonts, image resize/comp, js/css min

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const gulp = require('gulp');
+const image = require('gulp-image');
+
+gulp.task('image', function() {
+	gulp.src('./img/*')
+	.pipe(image({
+		pngquant: true,
+		optipng: false,
+		zopflipng: true,
+		jpegRecompress: true,
+		jpegoptim: false,
+		mozjpeg: false,
+		gifsicle: true,
+		svgo: true
+	}))
+	.pipe(gulp.dest('./compimg'));
+});
+
+gulp.task('default', ['image']);
